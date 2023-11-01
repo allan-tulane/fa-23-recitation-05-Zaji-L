3bfdb89c47567a07cee547375e892b5dabf94c64
Zaji-L <93162845+Zaji-L@users.noreply.github.com>
2023-11-01 22:34:40 +0000

    completed

--- answers.md
+++ answers.md
@@ -1,7 +1,7 @@
 # CMPS 2200 Recitation 6
 ## Answers
 
-**Name:**_________________________
+**Name:**Charlie Landman
 
 
 Place all written answers from `recitation-06.md` here for easier grading.
@@ -12,15 +12,15 @@ Place all written answers from `recitation-06.md` here for easier grading.
 
 File | Fixed-Length Coding | Huffman Coding | Huffman vs. Fixed-Length
 ----------------------------------------------------------------------
-f1.txt    |                     |                |
-alice29.txt    |                     |                |
-asyoulik.txt    |                     |                |
-grammar.lsp    |                     |                |
-fields.c    |                     |                |
+f1.txt         |    2144       |   826    |   413/1027 (reduced)
+alice29.txt    |    1187848    |   676374 |   338187/593924 (reduced)
+asyoulik.txt   |    1001432    |   606448 |   75806/125179 (reduced)
+grammar.lsp    |    29768      |   17356  |   4339/7442 (reduced)
+fields.c       |    89200      |   56206  |   28103/44600 (reduced)
 
 
 
 
 - **e.**
-
+If every character had the same frequency, Huffman Coding and Fixed-Length Coding would have the same cost, because Huffman Coding's advantage over Fixed-Length Coding is lost, meaning that the compression advantage is useless because all characters have the same encoding.
 
--- main.py
+++ main.py
@@ -1,60 +1,84 @@
 import math, queue
 from collections import Counter
 
-class TreeNode(object):
-    # we assume data is a tuple (frequency, character)
-    def __init__(self, left=None, right=None, data=None):
-        self.left = left
-        self.right = right
-        self.data = data
-    def __lt__(self, other):
-        return(self.data < other.data)
-    def children(self):
-        return((self.left, self.right))
-    
-def get_frequencies(fname):
-    ## This function is done.
-    ## Given any file name, this function reads line by line to count the frequency per character. 
-    f=open(fname, 'r')
-    C = Counter()
-    for l in f.readlines():
-        C.update(Counter(l))
-    return(dict(C.most_common()))
 
-# given a dictionary f mapping characters to frequencies, 
+class TreeNode(object):
+  # we assume data is a tuple (frequency, character)
+  def __init__(self, left=None, right=None, data=None):
+    self.left = left
+    self.right = right
+    self.data = data
+
+  def __lt__(self, other):
+    return (self.data < other.data)
+
+  def children(self):
+    return ((self.left, self.right))
+
+
+def get_frequencies(fname):
+  ## This function is done.
+  ## Given any file name, this function reads line by line to count the frequency per character.
+  f = open(fname, 'r')
+  C = Counter()
+  for l in f.readlines():
+    C.update(Counter(l))
+  return (dict(C.most_common()))
+
+
+# given a dictionary f mapping characters to frequencies,
 # create a prefix code tree using Huffman's algorithm
 def make_huffman_tree(f):
-    p = queue.PriorityQueue()
-    # construct heap from frequencies, the initial items should be
-    # the leaves of the final tree
-    for c in f.keys():
-        p.put(TreeNode(None,None,(f[c], c)))
+  p = queue.PriorityQueue()
+  # construct heap from frequencies, the initial items should be
+  # the leaves of the final tree
+  for c in f.keys():
+    p.put(TreeNode(None, None, (f[c], c)))
 
-    # greedily remove the two nodes x and y with lowest frequency,
-    # create a new node z with x and y as children,
-    # insert z into the priority queue (using an empty character "")
-    while (p.qsize() > 1):
-        # TODO
-        
-    # return root of the tree
-    return p.get()
+  # greedily remove the two nodes x and y with lowest frequency,
+  # create a new node z with x and y as children,
+  # insert z into the priority queue (using an empty character "")
+  while (p.qsize() > 1):
+    x = p.get()
+    y = p.get()
+    z = TreeNode(x, y, (x.data[0] + y.data[0], ""))
+    p.put(z)
+
+  # return root of the tree
+  return p.get()
+
 
 # perform a traversal on the prefix code tree to collect all encodings
 def get_code(node, prefix="", code={}):
-    # TODO - perform a tree traversal and collect encodings for leaves in code
-    pass
+  # TODO - perform a tree traversal and collect encodings for leaves in code
+  if node:
+    if node.data[1]:  # Check if the node is a leaf
+      code[node.data[1]] = prefix
+    get_code(node.left, prefix + "0", code)
+    get_code(node.right, prefix + "1", code)
+  return code
+
 
 # given an alphabet and frequencies, compute the cost of a fixed length encoding
 def fixed_length_cost(f):
-    # TODO
-    pass
+  fixed_length = 8  # Assuming 8 bits per character (ASCII)
+  total_cost = 0
+  for char, freq in f.items():
+    total_cost += freq * fixed_length
+  return total_cost
+
 
 # given a Huffman encoding and character frequencies, compute cost of a Huffman encoding
 def huffman_cost(C, f):
-    # TODO
-    pass
+  # TODO
+  total_cost = 0
+  for char, freq in f.items():
+    total_cost += freq * len(
+        C[char])  # C[char] is the Huffman encoding of the character
+  return total_cost
 
-f = get_frequencies('f1.txt')
+
+f = get_frequencies('fields.c')
 print("Fixed-length cost:  %d" % fixed_length_cost(f))
 T = make_huffman_tree(f)
 C = get_code(T)
